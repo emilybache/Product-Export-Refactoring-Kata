7b83892de63c600465fce017fc58c7d0fb0ee567
emilybache <emily.bache@praqma.com>
2018-08-21 11:38:27 +0200

    setting up the exercise without test coverage from the start

--- src/test/java/codingdojo/XMLExporterTest.java
+++ src/test/java/codingdojo/XMLExporterTest.java
@@ -12,8 +12,8 @@ public class XMLExporterTest {
 
     private ArrayList<Order> orders;
 
-    @Before
-    public void setUp() throws ParseException {
+    @Test
+    public void testSomething() throws Exception {
         Store nordstan = new Store("Nordstan", "4189");
 
         Product cherryBloom = new Product("Cherry Bloom", "LIPSTICK01", 30, new Price(14.99D, "USD"));
@@ -22,40 +22,16 @@ public class XMLExporterTest {
         Product eyelashCurler = new Product("Eyelash curler", "TOOL01", 100, new Price(19.99D, "USD"));
         Product wildRose = new Product("Wild Rose", "PURFUME01", 200, new Price(34.99D, "USD"));
         Product cocoaButter = new Product("Cocoa Butter", "SKIN_CREAM01", 250, new Price(10.99D, "USD"));
-
         nordstan.addStockedItems(cherryBloom, rosePetal, blusherBrush, eyelashCurler, wildRose, cocoaButter);
 
         // Store events add themselves to the stocked items at their store
         Product masterclass = new StoreEvent("Eyeshadow Masterclass", "EVENT01", nordstan, new Price(119.99D, "USD"));
         Product makeover = new StoreEvent("Makeover", "EVENT02", nordstan, new Price(149.99D, "USD"));
 
-        Order order1 = new Order("1234", Util.fromISO8601UTC("2018-09-01T13:24Z"), nordstan);
-        order1.addProducts(cherryBloom, cherryBloom, wildRose, cocoaButter, masterclass);
+        Order order = new Order("1234", Util.fromISO8601UTC("2018-09-01T13:24Z"), nordstan);
 
-        Order order2 = new Order("1235", Util.fromISO8601UTC("2017-08-31T14:02Z"), nordstan);
-        order2.addProducts(eyelashCurler, blusherBrush, wildRose, makeover);
-
-        orders = new ArrayList<Order>();
-        orders.add(order1);
-        orders.add(order2);
-    }
-
-    @Test
-    public void testFullExport() {
-        String xml = XMLExporter.exportFull(orders);
-        Approvals.verify(xml);
-    }
-
-    @Test
-    public void testExportHistory() {
-        String xml = XMLExporter.exportHistory(orders);
-        Approvals.verify(xml);
+        // TODO: Finish this test case
     }
 
 
-    @Test
-    public void testExportTax() throws Exception {
-        String xml = XMLExporter.exportTaxDetails(orders);
-        Approvals.verify(xml);
-    }
 }
