77727d42f697f3c9de3873f31942277e9d2afcb9
emilybache <emily.bache@praqma.com>
2018-11-02 09:44:10 +0100

    full coverage

--- Java/src/test/java/codingdojo/XMLExporterTest.java
+++ Java/src/test/java/codingdojo/XMLExporterTest.java
@@ -6,18 +6,37 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.Collection;
 
+import static codingdojo.SampleModelObjects.FlagshipStore;
+import static codingdojo.SampleModelObjects.OldOrder;
 import static codingdojo.SampleModelObjects.RecentOrder;
 
 public class XMLExporterTest {
 
     @Test
     public void exportFull() throws Exception {
-
-        Collection<Order> orders = Arrays.asList(RecentOrder);
+        Collection<Order> orders = Arrays.asList(RecentOrder, OldOrder);
         String xml = XMLExporter.exportFull(orders);
         Approvals.verifyXml(xml);
-
     }
 
+    @Test
+    public void exportHistory() throws Exception {
+        Collection<Order> orders = Arrays.asList(RecentOrder, OldOrder);
+        String xml = XMLExporter.exportHistory(orders);
+        Approvals.verifyXml(xml);
+    }
 
+    @Test
+    public void exportTaxDetails() throws Exception {
+        Collection<Order> orders = Arrays.asList(RecentOrder, OldOrder);
+        String xml = XMLExporter.exportTaxDetails(orders);
+        Approvals.verifyXml(xml);
+    }
+
+    @Test
+    public void exportStore() throws Exception {
+        Store store = FlagshipStore;
+        String xml = XMLExporter.exportStore(store);
+        Approvals.verifyXml(xml);
+    }
 }
