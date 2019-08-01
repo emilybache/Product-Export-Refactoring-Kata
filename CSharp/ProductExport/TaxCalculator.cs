1343d22fea09cfcec68952fa092da19b8c9b7cb4
Emily Bache <emily.bache@praqma.com>
2019-08-01 16:31:12 +0200

    recreate csharp project using xunit

--- CSharp/ProductExport/TaxCalculator.cs
+++ CSharp/ProductExport/TaxCalculator.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 
-namespace export
+namespace ProductExport
 {
     public class TaxCalculator
     {
--- CSharp/ProductExport/XmlExporter.cs
+++ CSharp/ProductExport/XmlExporter.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace export
+namespace ProductExport
 {
     public class XmlExporter
     {
