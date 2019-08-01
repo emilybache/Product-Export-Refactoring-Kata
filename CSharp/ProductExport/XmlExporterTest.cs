96f69a903d1ac6ff3037f14985cc1e5f27241432
Emily Bache <emily.bache@praqma.com>
2019-08-01 16:58:29 +0200

    added tests for csharp version

--- CSharp/ProductExport/XmlExporterTest.cs
+++ CSharp/ProductExport/XmlExporterTest.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using ApprovalTests;
 using ApprovalTests.Reporters;
 using Xunit;
@@ -6,16 +7,41 @@ using Xunit;
 
 namespace ProductExport
 {
+    [UseReporter(typeof(DiffReporter))]
     public class XmlExporterTest
     {
-
         [Fact]
-        public void TestSomething()
+        public void ExportFull()
         {
-            // TODO: finish writing this test
+            var orders = new List<Order>() {SampleModelObjects.RecentOrder, SampleModelObjects.OldOrder};
+            var xml = XmlExporter.ExportFull(orders);
+            Approvals.VerifyXml(xml);
         }
 
+        [Fact]
+        public void ExportTaxDetails()
+        {
+            var orders = new List<Order>() {SampleModelObjects.RecentOrder, SampleModelObjects.OldOrder};
+            var xml = XmlExporter.ExportTaxDetails(orders);
+            Approvals.VerifyXml(xml);
+        }
+
+        [Fact]
+        public void ExportStore()
+        {
+            var store = SampleModelObjects.FlagshipStore;
+            var xml = XmlExporter.ExportStore(store);
+            Approvals.VerifyXml(xml);
+        }
+
+        [Fact]
+        public void ExportHistory()
+        {
+            var orders = new List<Order>() {SampleModelObjects.RecentOrder, SampleModelObjects.OldOrder};
+            var xml = XmlExporter.ExportHistory(orders);
+            var regex = "createdAt=\"[^\"]+\"";
+            var report = Regex.Replace(xml, regex, "createdAt=\"2018-09-20T00:00Z\"");
+            Approvals.VerifyXml(report);
+        }
     }
-
-
 }
