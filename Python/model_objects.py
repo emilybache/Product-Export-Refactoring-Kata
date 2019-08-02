380f43a764397ce9e8f4d0208135e5a7523abba4
Emily Bache <emily.bache@praqma.com>
2019-08-02 16:17:54 +0200

    first attempt at python version

---
+++ Python/model_objects.py
@@ -0,0 +1,46 @@
+
+class ModelObject:
+    def save_to_database(self):
+        raise Exception("You shouldn't call save_to_database from a unit test")
+
+
+class Order(ModelObject):
+    def __init__(self, id, date, store, products):
+        self.id = id
+        self.date = date
+        self.store = store
+        self.products = products[:]
+
+
+class Price(ModelObject):
+    def __init__(self, amount, currency):
+        self.amount = amount
+        self.currency = currency
+
+
+class Product(ModelObject):
+    def __init__(self, name, id, weight, price):
+        self.name = name
+        self.id = id
+        self.weight = weight
+        self.price = price
+
+
+class Store(ModelObject):
+    """Represents a physical Store where you can go and buy products and attend events."""
+    def __init__(self, name, id, products=None):
+        self.name = name
+        self.id = id
+        self.products = products or []
+
+
+class StoreEvent(Product):
+    """Ticket to In-store event, eg makeover, eyeshadow masterclass
+    or beauty product launch evening reception"""
+    def __init__(self, name, store, id, price):
+        super().__init__(name, id, 0, price)
+        self.store = store
+        self.isEvent = True
+
+
+
