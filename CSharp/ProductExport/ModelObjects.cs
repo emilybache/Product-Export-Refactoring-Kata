cfd2bb571f7e2ee15428f33f15b4d870f0f2acbc
Peter Kofler <peter.kofler@code-cop.org>
2020-05-02 20:24:15 +0200

    C# add SaveToDatabase, make model objects not writable.

--- CSharp/ProductExport/ModelObjects.cs
+++ CSharp/ProductExport/ModelObjects.cs
@@ -6,17 +6,17 @@ namespace ProductExport
 {
     public class StoreEvent : Product
     {
-        public StoreEvent(string name, string type, Store store, Price price) : base(name, type, 0, price)
+        public StoreEvent(string name, string type, Store location, Price price) : base(name, type, 0, price)
         {
-            SetLocation(store);
+            SetLocation(location);
         }
 
-        private Store Store { get; set; }
+        private Store Location { get; set; }
 
         public void SetLocation(Store store)
         {
-            Store = store;
-            store.AddStockedItems(this);
+            Location = store;
+            store.AddStoreEvent(this);
         }
 
         public override bool IsEvent()
@@ -35,38 +35,48 @@ namespace ProductExport
             Price = price;
         }
 
-        public string Name { get; set; }
-        public string Id { get; set; }
-        public int Weight { get; set; }
-        public Price Price { get; set; }
+        public string Name { get; protected set; }
+        public string Id { get; protected set; }
+        public int Weight { get; protected set; }
+        public Price Price { get; protected set; }
 
         public virtual bool IsEvent()
         {
             return false;
         }
+
+        public void SaveToDatabase()
+        {
+            throw new NotImplementedException("missing from this exercise - shouldn't be called from a unit test");
+        }
     }
 
     public class Price
     {
-        public Price(double amount, string currency)
+        public Price(double amount, string currencyCode)
         {
             Amount = amount;
-            Currency = currency;
+            CurrencyCode = currencyCode;
         }
 
-        public double Amount { get; set; }
-        public string Currency { get; set; }
+        public double Amount { get; private set; }
+        public string CurrencyCode { get; private set; }
 
-        public double GetAmountInCurrency(string currency)
+        public double GetAmountInCurrency(string currencyCode)
         {
-            return Amount;
+            if (CurrencyCode == currencyCode)
+                return Amount;
+            else
+            {
+                throw new NotImplementedException("shouldn't call this from a unit test, it will do a slow db lookup");
+            }
         }
     }
 
     public class Store
     {
         public readonly IList<Product> Stock = new List<Product>();
-        
+
         public Store(string name, string id, Product[] products)
         {
             this.Name = name;
@@ -74,30 +84,39 @@ namespace ProductExport
             this.AddStockedItems(products);
         }
 
-        public string Name { get; set; }
-        public string Id { get; set; }
-
+        public string Name { get; private set; }
+        public string Id { get; private set; }
 
         public void AddStockedItems(params Product[] products)
         {
             foreach (var product in products) Stock.Add(product);
         }
+
+        public void AddStoreEvent(StoreEvent storeEvent)
+        {
+            Stock.Add(storeEvent);
+        }
+
+        public void SaveToDatabase()
+        {
+            throw new NotImplementedException("missing from this exercise - shouldn't be called from a unit test");
+        }
     }
 
     public class Order
     {
-        
-        public Order(string id, DateTime dateTime, Store flagshipStore, Product[] products)
+
+        public Order(string id, DateTime dateTime, Store store, Product[] products)
         {
             this.Id = id;
             this.Date = dateTime;
-            this.Store = flagshipStore;
+            this.Store = store;
             this.AddProducts(products);
         }
 
-        public string Id { get; set; }
-        public DateTime Date { get; set; }
-        public Store Store { get; set; }
+        public string Id { get; private set; }
+        public DateTime Date { get; private set; }
+        public Store Store { get; private set; }
 
         public readonly IList<Product> Products = new List<Product>();
 
@@ -109,6 +128,11 @@ namespace ProductExport
         public void AddProducts(params Product[] products)
         {
             foreach (var product in products) Products.Add(product);
+        }
+
+        public void SaveToDatabase()
+        {
+            throw new NotImplementedException("missing from this exercise - shouldn't be called from a unit test");
         }
     }
 }
