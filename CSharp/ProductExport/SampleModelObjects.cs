c2d801bb47c955dd184858561c4de040d07490aa
Peter Kofler <peter.kofler@code-cop.org>
2020-05-02 19:43:07 +0200

    C# format empty lines.

--- CSharp/ProductExport/SampleModelObjects.cs
+++ CSharp/ProductExport/SampleModelObjects.cs
@@ -11,7 +11,7 @@ namespace ProductExport
     /// </summary>
     public class SampleModelObjects
     {
-        
+
         public static Product CherryBloom = new Product("Cherry Bloom", "LIPSTICK01", 30, new Price(14.99D, "USD"));
         public static Product RosePetal = new Product("Rose Petal", "LIPSTICK02", 30, new Price(14.99D, "USD"));
         public static Product BlusherBrush = new Product("Blusher Brush", "TOOL01", 50, new Price(24.99D, "USD"));
@@ -20,14 +20,13 @@ namespace ProductExport
         public static Product CocoaButter = new Product("Cocoa Butter", "SKIN_CREAM01", 250, new Price(10.99D, "USD"));
 
         public static Store FlagshipStore = new Store("Nordstan", "4189", new Product[] { CherryBloom });
-        
+
         // Store events add themselves to the stocked items at their store
         public static Product Masterclass = new StoreEvent("Eyeshadow Masterclass", "EVENT01", FlagshipStore, new Price(119.99D, "USD"));
         public static Product Makeover = new StoreEvent("Makeover", "EVENT02", FlagshipStore, new Price(149.99D, "USD"));
 
-        public static Order RecentOrder = new Order("1234", Util.FromIsoDate("2018-09-01T00:00Z"), 
-            SampleModelObjects.FlagshipStore, new Product[] {Makeover});
+        public static Order RecentOrder = new Order("1234", Util.FromIsoDate("2018-09-01T00:00Z"),
+            SampleModelObjects.FlagshipStore, new Product[] { Makeover });
     }
 
-    
 }
