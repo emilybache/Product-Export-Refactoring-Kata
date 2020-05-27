ac4746ff9aa3bfd8a6f06027c4b9f337de76a218
emily <emily@bacheconsulting.com>
2020-05-27 20:05:45 +0200

    reset to starting position

--- Cpp/test/XMLExporterTest.cpp
+++ Cpp/test/XMLExporterTest.cpp
@@ -6,59 +6,20 @@
 
 #include <vector>
 
-std::string scrubDate(const std::string& input)
-{
-    static const std::regex regex("createdAt='[^']+'");
-
-    int matchNumber = 1;
-    auto result = input;
-    std::smatch m;
-    if (std::regex_search(result, m, regex))
-    {
-        auto guid_match = m.str();
-        auto replacement = "createdAt='date_" + std::to_string(matchNumber) + "'";
-        result = ApprovalTests::StringUtils::replaceAll(result, guid_match, replacement);
-        matchNumber += 1;
-    }
-    return result;
-}
-
+// This is a convenience function that writes an xml string into a file with name <something>.xml
+// it also inserts some linebreaks to make the file easier to read and diff.
 void verifyXml(std::string xml, ApprovalTests::Options options=ApprovalTests::Options()) {
-    //auto path = "/Users/emily/Library/Application\\ Support/JetBrains/Toolbox/apps/CLion/ch-0/201.7223.86/CLion.app";
-    //auto args = "diff {Received} {Approved}";
-    //auto reporter = ApprovalTests::CustomReporter::create(path, args);
     xml = ApprovalTests::StringUtils::replaceAll(xml, ">", ">\n");
     ApprovalTests::Approvals::verify(xml, options.fileOptions().withFileExtension(".xml"));
 }
-
 
 
 TEST_CASE("XMLExporter")
 {
     make_sample_model_objects();
 
-    SECTION("export store") {
-        auto xml = XMLExporter().export_store(FlagshipStore);
-        verifyXml(xml);
-    }
+    // TODO: write this test case
 
-   auto orders = std::vector<Order*>();
-   orders.push_back(RecentOrder);
-   orders.push_back(OldOrder);
-
-   SECTION("export full") {
-        auto xml = XMLExporter().export_full(orders);
-        verifyXml(xml);
-    }
-    SECTION("export tax details") {
-        auto xml = XMLExporter().export_tax_details(orders);
-        verifyXml(xml);
-   }
-   SECTION("export history") {
-        auto xml = XMLExporter().export_history(orders);
-        auto options = ApprovalTests::Options().withScrubber(scrubDate);
-        verifyXml(xml, options);
-   }
 }
 
 
