a2e9a7309f77b5ef66dec33b7eda3072a699fbad
emily <emily@bacheconsulting.com>
2021-04-25 10:10:53 +0200

    adding useful helper method

--- c_catch2/test-catch2/XMLExporterTest.cpp
+++ c_catch2/test-catch2/XMLExporterTest.cpp
@@ -6,9 +6,15 @@ extern "C"
 #include "LinkedList.h"
 #include "SampleModelObjects.h"
 #include "XMLExporter.h"
-
 }
 
+void verifyXml(std::string xml, ApprovalTests::Options options = ApprovalTests::Options()) {
+    // Not as good as actual formatting and layout but better than nothing.
+    xml = ApprovalTests::StringUtils::replaceAll(xml, ">", ">\n");
+
+    ApprovalTests::Approvals::verify(xml,
+                                     options.fileOptions().withFileExtension(".xml"));
+}
 
 TEST_CASE ("XMLExporter") {
     make_sample_model_objects();
