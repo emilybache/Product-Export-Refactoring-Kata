509ace029aaaa1420adea8accfae0e38571e1d0b
emily <emily@bacheconsulting.com>
2022-06-14 17:14:25 +0200

    add useful VerifyXml functionality

--- CSharp/ProductExport/XmlExporterTest.cs
+++ CSharp/ProductExport/XmlExporterTest.cs
@@ -1,5 +1,10 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using VerifyTests;
 using VerifyXunit;
 using Xunit;
 
@@ -12,8 +17,38 @@ namespace ProductExport
         public Task Something()
         {
             // TODO: finish writing this test
-            var something = "";
-            return Verifier.Verify(something);
+            var xml = "";
+            return VerifyXml(xml);
         }
+        
+        private Task VerifyXml(string xml)
+        {
+            var settings = new VerifySettings();
+            return VerifyXml(xml, settings);
+        }
+
+        private static Task VerifyXml(string xml, VerifySettings settings)
+        {
+            try
+            {
+                settings.UseExtension("xml");
+                var xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(xml);
+                return Verifier.Verify(ToIndentedString(xmlDoc), settings);
+            }
+            catch (Exception e)
+            {
+                return Verifier.Verify(xml);
+            }
+        }
+
+        static string ToIndentedString( XmlDocument doc )
+        {
+            var stringWriter = new StringWriter(new StringBuilder());
+            var xmlTextWriter = new XmlTextWriter(stringWriter) {Formatting = Formatting.Indented};
+            doc.Save( xmlTextWriter );
+            return stringWriter.ToString();
+        }
+
     }
 }
