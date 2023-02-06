a3eb26a6421045e3d623922b6f2da145681e6c11
emily <emily@bacheconsulting.com>
2023-02-06 11:22:39 +0100

    remove redundant regex

--- Java/src/test/java/codingdojo/XMLExporterTest.java
+++ Java/src/test/java/codingdojo/XMLExporterTest.java
@@ -3,16 +3,12 @@ package codingdojo;
 import org.approvaltests.Approvals;
 import org.approvaltests.core.Options;
 import org.approvaltests.scrubbers.DateScrubber;
-import org.approvaltests.scrubbers.RegExScrubber;
+import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.Collection;
 
-import static codingdojo.SampleModelObjects.FlagshipStore;
-import static codingdojo.SampleModelObjects.OldOrder;
-import static codingdojo.SampleModelObjects.RecentOrder;
-
-import org.junit.jupiter.api.Test;
+import static codingdojo.SampleModelObjects.*;
 
 public class XMLExporterTest {
 
@@ -41,7 +37,6 @@ public class XMLExporterTest {
     public void exportHistory() throws Exception {
         Collection<Order> orders = Arrays.asList(RecentOrder, OldOrder);
         String xml = XMLExporter.exportHistory(orders);
-        String regex = "createdAt='[^']+'";
         Approvals.verifyXml(xml, new Options().withScrubber(
                 DateScrubber.getScrubberFor("2022-06-21T11:29Z")
         ));
