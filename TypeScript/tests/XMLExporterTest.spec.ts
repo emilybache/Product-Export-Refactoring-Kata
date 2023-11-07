47bf9766f3798f6985f863e18051ab57ebafe5c9
Peter Kofler <peter.kofler@code-cop.org>
2023-11-07 21:01:24 +0100

    TypeScript: Snapshot tests.

--- TypeScript/tests/XMLExporterTest.spec.ts
+++ TypeScript/tests/XMLExporterTest.spec.ts
@@ -1,11 +1,37 @@
+import { Order } from "../src/Order";
+import { Util } from "../src/Util";
+import { XMLExporter } from "../src/XMLExporter";
+
+import { FlagshipStore, Makeover, CherryBloom } from "./SampleModelObjects";
+
+const RecentOrder = new Order("1234", Util.fromIsoDate("2018-09-01T00:00Z"), FlagshipStore, [Makeover]);
+const OldOrder = new Order("1235", Util.fromIsoDate("2017-09-01T00:00Z"), FlagshipStore, [CherryBloom]);
 
 describe("XMLExporter", () => {
 
-    it("Something", () => {
+    it("exportFull", () => {
+        const orders = [RecentOrder, OldOrder];
+        const xml = XMLExporter.exportFull(orders);
+        expect(xml).toMatchSnapshot();
+    });
 
-        // TODO: implement this test
+    it("exportTaxDetails", () => {
+        const orders = [RecentOrder, OldOrder];
+        const xml = XMLExporter.exportTaxDetails(orders);
+        expect(xml).toMatchSnapshot();
+    });
 
-        // expect(xml).toMatchSnapshot(); // Use snapshot testing
+    it("exportStore", () => {
+        const store = FlagshipStore;
+        const xml = XMLExporter.exportStore(store);
+        expect(xml).toMatchSnapshot();
+    });
+
+    it("exportHistory", () => {
+        const orders = [RecentOrder, OldOrder];
+        let xml = XMLExporter.exportHistory(orders);
+        xml = xml.replace(/createdAt='[^']+'/, "createdAt='2018-09-20T00:00Z'");
+        expect(xml).toMatchSnapshot();
     });
 
 });
